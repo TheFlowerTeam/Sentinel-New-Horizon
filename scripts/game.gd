292f7577cf8f51192ee3bd470ca1481d4ee4bb45
xeno178531 <kubekkakao6@gmail.com>
2026-03-24 19:48:19 +0100

    Łączenie sygnałow i game pause przy uruchomionym menu

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,1 +1,9 @@
 extends Node2D
+
+func _ready() -> void:
+	GameEvents.menu_switched.connect(_on_menu_switched)
+	
+func _on_menu_switched(is_open:bool) -> void:
+	get_tree().paused = is_open
+	pass
+	
---
+++ scripts/game_events.gd
@@ -0,0 +1,5 @@
+extends Node
+
+
+@warning_ignore("unused_signal")
+signal menu_switched(is_open:bool)
