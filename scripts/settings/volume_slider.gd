37c4caefbe3d53ed337c230504fa46630fdb18a9
xeno178531 <kubekkakao6@gmail.com>
2026-03-31 20:16:52 +0200

    SCRUM-10-'Połączono volume sliders z SettingsManagerem.'

--- scripts/settings/volume_slider.gd
+++ scripts/settings/volume_slider.gd
@@ -3,15 +3,13 @@ extends Panel
 #region Zmienne
 
 @onready var label:Label = %Label
-@export var text:String
 @export_enum("Master", "Music", "SFX") var BUS:String
 
 #endregion
 
 func _ready() -> void:
-	label.text = text
+	label.text = BUS
 
 func _on_h_slider_value_changed(value: float) -> void:
-	var bus_id = AudioServer.get_bus_index(BUS)
-	var db = linear_to_db(value)
-	AudioServer.set_bus_volume_db(bus_id, db)
+	var data = {"value": value, "bus":BUS}
+	SettingsManager._set_setting("Audio", data)
