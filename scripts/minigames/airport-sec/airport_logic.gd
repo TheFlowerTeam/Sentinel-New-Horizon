35be77e96ca553d1cc35ced53211d099cb1e3bb5
xeno178531 <kubekkakao6@gmail.com>
2026-04-04 11:08:46 +0200

    SCRUM-17 Dodano twarde typowanie.

--- scripts/minigames/airport-sec/airport_logic.gd
+++ scripts/minigames/airport-sec/airport_logic.gd
@@ -1,8 +1,8 @@
 extends Node
 
-@onready var character_1: OptionButton = $Character1
-@onready var checker: OptionButton = $Checker
-@onready var character_2: OptionButton = $Character2
+@onready var character_1:	OptionButton = $Character1
+@onready var checker:		OptionButton = $Checker
+@onready var character_2:	OptionButton = $Character2
 
 var path_to_data = "res://assets/resources/airport.json"
 var file = FileAccess.open(path_to_data, FileAccess.READ)
@@ -16,13 +16,15 @@ func _ready() -> void:
 	for npc in npc_list:
 		var text = "name: %s\n surname: %s\nid: %s" % [npc["name"], npc["surname"], npc["id"]]
 		character_1.add_item(text)	
-func random_npc_data() -> Array:
-	var npc:Array
+
+
+func random_npc_data() -> Array[Dictionary]:
+	var npc:Array[Dictionary]
 	
 	for i in range(10):
 		var person:Dictionary 
-		var category:String = option.pick_random()
-		var list:Array  	= data["names"][category]
+		var category:String 			= option.pick_random()
+		var list:Array[Dictionary]  	= data["names"][category]
 		person["name"]  	= list.pick_random()
 
 		list 				= data["surnames"]
@@ -34,23 +36,23 @@ func random_npc_data() -> Array:
 	
 	return npc
 
-func anomaly(original:Array) -> Array:
-	var anomaly_list:Array	= []
+func anomaly(original:Array[Dictionary]) -> Array[Dictionary]:
+	var anomaly_list:Array[Dictionary]	= []
 	var size:int		= original.size()
 	var percent:int		= floor(randf_range(0.1, 0.4) * size)
 	
 	for anomaly_char in range(percent):
 		var character:Dictionary = original.pick_random().duplicate(true)
-		var options:Array = ["names", "surnames", "id"]
+		var options:Array[String] = ["names", "surnames", "id"]
 		
 		for anomalies in randi_range(1, 4):
-			var selector = options.pick_random()
+			var selector:String = options.pick_random()
 			
 			if selector == "names":
-				var mutation:Array = data["names"]["neutral"]
+				var mutation:Array[Dictionary] = data["names"]["neutral"]
 				character["name"] = mutation.pick_random()
 			elif selector == "surnames":
-				var mutation:Array = data["surnames"]
+				var mutation:Array[Dictionary] = data["surnames"]
 				character["surname"] = mutation.pick_random()
 			else:
 				character["id"] = randi_range(10000000, 99999999)
@@ -59,8 +61,6 @@ func anomaly(original:Array) -> Array:
 	
 	return	anomaly_list
 
-func check(first:Dictionary, second:Dictionary, comparator:String):
-	if first[comparator] == second[comparator]:
-		print("Correct")
-	else:
-		print("Anomaly")
+func check():
+	pass
+	
