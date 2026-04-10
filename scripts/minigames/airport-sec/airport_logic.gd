9033a18993615226da1a61601c0b6cfe38c00f70
xeno178531 <kubekkakao6@gmail.com>
2026-04-10 21:19:54 +0200

    Dodano karte dokumentow, i szablony. Dodano klase NPC. Dodano System losowania danych. Foldery NPC.

--- scripts/minigames/airport-sec/airport_logic.gd
+++ scripts/minigames/airport-sec/airport_logic.gd
@@ -1,88 +1,53 @@
+# Kod wspomagany AI
 extends Node
 
-@onready var character_1:	OptionButton 	= $Character1
-@onready var checker:		OptionButton 	= $Checker
-@onready var character_2:	OptionButton 	= $Character2
-@onready var correct_name: 	Label			= $CorrectName
-@onready var incorrect_name_2: 	Label 		= $IncorrectName2
-@onready var button: 			Button		= $Button
 
-
-#region zmienne
-var path_to_data = "res://assets/resources/person_data.json"
-var data_file = FileAccess.open(path_to_data, FileAccess.READ)
-var data = JSON.parse_string(data_file.get_as_text())
-
-var anomalies_file = FileAccess.open("res://assets/resources/anomalies.json", FileAccess.READ)
-var anomalies:Array = JSON.parse_string(anomalies_file.get_as_text())
+#region NPC
+@onready var texture_npc: Sprite2D = %TextureNPC
+@onready var sound_npc: AudioStreamPlayer2D = %SoundNPC
 #endregion
 
-var option:Array		= ["male", "female", "neutral"]
-var npc_list:Array
+#region Document
+@onready var personal_document: Label = %PersonalDocument
+@onready var bday_document: Label = %BdayDocument
+@onready var id_document: Label = %IdDocument
+#endregion
 
-func _ready() -> void:
-	npc_list		= random_npc_data()
-	for npc in npc_list:
-		var text = "name: %s\n surname: %s\n id: %s" % [npc["name"], npc["surname"], npc["id"]]
-		character_1.add_item(text)	
-		print(npc)
+#region Pasport
+@onready var name_pasport: Label = %NamePasport
+@onready var surname_pasport: Label = %SurnamePasport
+@onready var id_pasport: Label = %IdPasport
+#endregion
+
+#region AdditionalFunctions
+func display_current_npc() -> void:
+	var npc: NPC = npc_list[current_npc_index]
+	personal_document.text = "Imię i nzawisko: %s %s" % [npc.npc_name, npc.npc_surname]
+	bday_document.text = "Urodzony/a: %s" % npc.npc_birthday
+	id_document.text = "Numer ID: %s" % npc.npc_id
+	var path_to_img:String = npc.npc_img
+	texture_npc.texture = load(path_to_img)
 	
-func random_npc_data() -> Array[Dictionary]:
-	var npc:Array[Dictionary]
-	
-	for i in range(10):
-		var person:Dictionary 
-		var category:String = option.pick_random()
-		var names:Array	= data["names"][category]
-		person["name"]  	= names.pick_random()
-
-		var surnames:Array	= data["surnames"]
-		surnames 				= data["surnames"]
-		person["surname"]	= surnames.pick_random()
-		
-		var year	= randi_range(1970, 2005)
-		var month	= randi_range(1, 12)
-		var day		= randi_range(1, 28)
-		person["bday"]		=  "%d.%d.%d" % [day, month, year]
-
-		person["id"]		= caesar(person["name"], person["surname"], person["bday"])
-		person["anomalies"]	= anomaly()
-		person["key"]	= i
-
-		npc.append(person)
-	
-	return npc
-
-func anomaly() -> Array:
-	var list:Array = []
-	var pool:Array = anomalies
-	if randf() < 0.35:
-		var cards = randi_range(0, 3)
-		pool.shuffle()
-		list = pool.slice(0, cards)
-		
+func generate_npc_list(count: int) -> Array:
+	var list: Array = []
+	for i in range(count):
+		var npc = NPC.new()
+		npc.generate_npc()
+		npc.npc_debug = i
+		list.append(npc)
 	return list
 
-func caesar(first:String, second:String, id:String) -> String:
-	var shift:int		= int(id[0]) # izoluje przesuniecie z ID
-	var f_char:String	= first[0].to_upper() 
-	var s_char:String	= second[0].to_upper()	
-	var output:String	= char((ord(f_char) - 65 + shift) % 26 + 65) + \
-						  char((ord(s_char) - 65 + shift) % 26 + 65) # Szyfratort
-	output += str(shift)
-	
-	var id_code:String = str(randi_range(0, 99999))
-	id_code = id_code.pad_zeros(5)
-	output += id_code # dopełnienie zerami od prawej do 5 cyfr
-	
-	return output	
+#endregion
 
-func anomaly_manager(character:Dictionary) -> void:
-	var text = "name: %s\nsurname: %s\nbirth day:%s\nid: %s" % [character["name"], character["surname"],character["bday"], character["id"]]
-	correct_name.text = text
-	
+
+var npc_list: Array = []
+var current_npc_index: int = 0
+
+func _ready() -> void:
+	#load_game_data()
+	npc_list = generate_npc_list(10)
+	display_current_npc()
+
 func _on_button_pressed() -> void:
-	var x = 0
-	var char = npc_list[x]	
-	anomaly_manager(char)
-	x += 1
+	current_npc_index = (current_npc_index + 1) % npc_list.size()
+	display_current_npc()
---
+++ scripts/minigames/airport-sec/npc_data.gd
@@ -0,0 +1,96 @@
+# Kod wspomagany AI
+# ================= 
+# Klasa dla NPC
+
+class_name NPC
+extends Resource
+
+
+#region DaneNPC
+var npc_img:		String
+var npc_name:		String
+var npc_surname:	String
+var npc_birthday:	String
+var npc_id:			String
+var npc_anomalies:	Array
+var npc_debug:		int
+#endregion
+
+#region ZmiennePlików
+var _path_to_data 	= "res://assets/resources/person_data.json"
+var _data_file 		= FileAccess.open(_path_to_data, FileAccess.READ)
+var _data 			= JSON.parse_string(_data_file.get_as_text())
+var _anomalies_file = FileAccess.open("res://assets/resources/anomalies.json", FileAccess.READ)
+var _anomalies:Array = JSON.parse_string(_anomalies_file.get_as_text())
+var _option:Array	= ["male", "female", "neutral"]
+#endregion
+
+#region FunkcjeGłówne
+
+func string() -> String:
+	return "NPC %s %s, bday: %s, id: %s, anomalies: %s" % [
+		npc_name, npc_surname, npc_birthday, npc_id, npc_anomalies
+	]
+	
+func generate_npc() -> void:
+		var category:String = _option.pick_random()
+		npc_name = _data["names"][category].pick_random()
+		npc_surname	= _data["surnames"].pick_random()
+		
+		var year	= randi_range(1970, 2005)
+		var month	= randi_range(1, 12)
+		var day		= randi_range(1, 28)
+		npc_birthday		=  "%d.%d.%d" % [day, month, year]
+
+		npc_id		= _caesar(npc_name, npc_surname, npc_birthday)
+		npc_anomalies	= _anomaly()
+		npc_img = _image(category)
+#endregion
+
+#region FunkcjePomocnicze
+
+func _image(category:String) -> String:
+	var path = "res://assets/assets/airport-sec/npc" 
+	var img_path = ""
+	var img_list:PackedStringArray
+	var correct_imgs:Array[String] = []
+	if category == 'male' || category == 'female':
+		path += "/%s" % category
+	else:
+		path += "/%s" % ["male", "female"].pick_random()
+			
+	var file = DirAccess
+	file = DirAccess.open(path)
+	img_list = file.get_files() 
+	
+	if !img_list.is_empty():
+		for f in img_list:
+			pass
+			if f.ends_with(".png"):
+				correct_imgs.append(f)
+		img_path = "%s/%s" % [path, correct_imgs.pick_random()]
+	return img_path
+
+func _caesar(first:String, second:String, id:String) -> String:
+	var shift:int		= int(id[0]) # izoluje przesuniecie z ID
+	var f_char:String	= first[0].to_upper() 
+	var s_char:String	= second[0].to_upper()	
+	var output:String	= char((ord(f_char) - 65 + shift) % 26 + 65) + \
+						  char((ord(s_char) - 65 + shift) % 26 + 65) # Szyfratort
+	output += str(shift)
+	
+	var id_code:String = str(randi_range(0, 99999))
+	id_code = id_code.pad_zeros(5)
+	output += id_code # dopełnienie zerami od prawej do 5 cyfr
+	
+	return output
+	
+func _anomaly() -> Array:
+		var list:Array = []
+		var pool:Array = _anomalies
+		if randf() < 0.35:
+			var cards = randi_range(0, 3)
+			pool.shuffle()
+			list = pool.slice(0, cards)
+		return list
+#endregion
