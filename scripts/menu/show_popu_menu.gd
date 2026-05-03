5aeeaf7eccff442da2cfddbe11ee0959ccf5adbc
xeno178531 <kubekkakao6@gmail.com>
2026-05-03 21:15:33 +0200

    Naprawiono kamere

--- scripts/menu/show_popu_menu.gd
+++ scripts/menu/show_popu_menu.gd
@@ -7,6 +7,8 @@ func _gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
 			get_child(0).visible = true
+			get_tree().paused = true
 			
 func _menu_closed() -> void:
 	get_child(0).visible = false
+	get_tree().paused = false
